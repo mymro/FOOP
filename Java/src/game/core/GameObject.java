aa8854695841ffa27f12ed091fd88b91760ef6d7
abbas <abbasulusoy@hotmail.com>
2018-06-15 16:34:00 +0200

    Test

--- Java/src/game/core/GameObject.java
+++ Java/src/game/core/GameObject.java
@@ -7,22 +7,26 @@ import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-public abstract class GameObject implements Serializable{
+/**
+ * abstract Game-Object define the parent game-object, then I will be added children for this object
+ *
+ */
+public abstract class GameObject implements Serializable {
     private GameObject parent;
     private LinkedList<GameObject> children;
     private int layer;
     private Main.GameSystem game_system;
     private double pos_x, pos_y;
 
-    public GameObject(){
+    public GameObject() {
         this(0);
     }
 
-    public GameObject(int layer){
+    public GameObject(int layer) {
         this(layer, 0, 0);
     }
 
-    public GameObject(int layer, double x,  double y){
+    public GameObject(int layer, double x, double y) {
         children = new LinkedList<>();
         this.layer = layer;
         game_system = Main.GameSystem.getInstance();
@@ -30,79 +34,144 @@ public abstract class GameObject implements Serializable{
         pos_y = y;
     }
 
-    public void update(){
-        for(GameObject go : children){
+    /**
+     * update game object set the position X and Y
+     */
+    public void update() {
+        for (GameObject go : children) {
             go.update();
         }
     }
 
+    /**
+     * draw the game objects to the graphicsContexts
+     *
+     * @param graphicsContext
+     */
     public void draw(GraphicsContext graphicsContext) {
-        for(GameObject go : children){
+        for (GameObject go : children) {
             go.draw(graphicsContext);
         }
     }
 
-    public final void attach(GameObject go){
-        if(go.parent != null){
+    /**
+     * attach game-object to parent object
+     * game object should have parent object
+     *
+     * @param go
+     */
+    public final void attach(GameObject go) {
+        if (go.parent != null) {
             go.parent.detach(go);
         }
         ListIterator<GameObject> it = children.listIterator();
         boolean child_added = false;
-        while (it.hasNext()){
-            if (it.next().layer > go.layer){
+        while (it.hasNext()) {
+            if (it.next().layer > go.layer) {
                 it.previous();
                 it.add(go);
                 child_added = true;
                 break;
             }
         }
-        if(!child_added){
+        if (!child_added) {
             children.add(children.size(), go);
         }
         go.onAttach(this);
     }
 
-    public void onAttach(GameObject parent){
+
+    /**
+     * define the parent for game-object
+     *
+     * @param parent
+     */
+    public void onAttach(GameObject parent) {
         this.parent = parent;
     }
 
-    public final void detach(GameObject go){
+    /**
+     * remove game object from children
+     *
+     * @param go
+     */
+    public final void detach(GameObject go) {
         children.remove(go);
         go.onDetach();
     }
 
-    public void onDetach(){
+    /**
+     * detach parent game-object
+     */
+    public void onDetach() {
         this.parent = null;
     }
 
-    public int getLayer(){
+    /**
+     * get layet of game-object
+     * @return
+     */
+    public int getLayer() {
         return layer;
     }
 
+    /**
+     * get parent of game-object
+     * @return parent
+     */
     public GameObject getParent() {
         return parent;
     }
 
-    public double getPos_x(){
+    /**
+     * get position x
+     *
+     * @return double position x
+     */
+    public double getPos_x() {
         return pos_x;
     }
 
-    public void setPos_x(double x){
+    /**
+     * set position x after update game-object
+     *
+     * @param x
+     */
+    public void setPos_x(double x) {
         pos_x = x;
     }
 
-    public double getPos_y(){
+    /**
+     * get position y after update game-object
+     *
+     * @return y
+     */
+    public double getPos_y() {
         return pos_y;
     }
 
-    public void setPos_y(double y){
+    /**
+     * set position x after update game-object
+     *
+     * @param y
+     */
+    public void setPos_y(double y) {
         pos_y = y;
     }
 
-    public Main.GameSystem getGame_system(){
+
+    /**
+     * get game system singleton object
+     * @return
+     */
+    public Main.GameSystem getGame_system() {
         return game_system;
     }
 
+    /**
+     * get children of parent game-object
+     * @return
+     */
     public LinkedList<GameObject> getChildren() {
         return children;
     }
