13ec67ead1992bb4e206ee7da929fd3708f3e793
abbas <abbasulusoy@hotmail.com>
2018-06-15 13:29:03 +0200

    Flag documentation

--- Java/src/game/core/Flag.java
+++ Java/src/game/core/Flag.java
@@ -1,7 +1,5 @@
 package game.core;
 
-import game.game.objects.Robot;
-
 public abstract class Flag extends GameObject implements FModifier {
 
     public enum flag_type {
@@ -13,6 +11,18 @@ public abstract class Flag extends GameObject implements FModifier {
     protected int dim_x;
     protected int dim_y;
 
+    /**
+     * create new Flag with layer, position and dimension
+     * The flag help the robots to find the path
+     * with the position x und y will be set to coordinate
+     * the dimensions are for the area of flag
+     * @param layer
+     * @param x
+     * @param y
+     * @param dim_x
+     * @param dim_y
+     * @throws IllegalArgumentException
+     */
     public Flag(int layer, int x, int y, int dim_x, int dim_y)throws IllegalArgumentException{
         super(layer, x, y);
         if(x<0 || x > dim_x || y < 0 || y > dim_y){
