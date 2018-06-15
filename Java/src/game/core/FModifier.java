42e5221381816280e9090084d4fc3ad6b63c24d0
abbas <abbasulusoy@hotmail.com>
2018-06-15 13:49:47 +0200

    Comment FModifier

--- Java/src/game/core/FModifier.java
+++ Java/src/game/core/FModifier.java
@@ -2,5 +2,13 @@ package game.core;
 
 public interface FModifier  {
 
+    /**
+     * it is influence to find the path.
+     * it will be change the path with the position x and y
+     * and will used a functional recursive call
+     * @param x
+     * @param y
+     * @return double value
+     */
     public double getFModifierAt(double x, double y);
 }
